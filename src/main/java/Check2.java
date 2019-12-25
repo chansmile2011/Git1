fcf886bef64b6988ee44d4fc789f8e82a95fafc3
chansmile2011 <chan.smile2011@gmail.com>
2019-12-25 19:41:06 +0530

    test3

---
+++ src/main/java/Check2.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+import org.testng.annotations.Test;
+
+public class Check2 {
+	
+	
+	@Test
+	void Tried()
+	{
+		
+	System.out.println("Good Morning Madam");
+	
+	}
+
+
+}
