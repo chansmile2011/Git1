e69c997f82906cccb4312d5e83f5acff760add52
chansmile2011 <chan.smile2011@gmail.com>
2019-12-27 10:30:11 +0530

    changes done

---
+++ src/test/java/Justcheck/Check10.java
@@ -0,0 +1,19 @@
+package Justcheck;
+
+import java.time.LocalDateTime;
+
+import org.testng.annotations.Test;
+
+public class Check10 {
+	
+	@Test
+	void Tried()
+	{
+				
+		System.out.println("Hello How are You !!!!!!!!!!!!!");
+		
+		System.out.println("Welcome");
+	}
+
+
+}
